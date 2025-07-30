0e7de4e89bedd5716a8f92acb169cc5450d1cb8d
shashikant borgavakar <borgavakarshashikant@gmail.com>
2025-07-30 17:48:48 +0000

    npm run dev
    > nextn@0.1.0 dev
    > next dev --turbopack -p 9002
    
       ▲ Next

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,6 +1,5 @@
 import {genkit} from 'genkit';
-import {groq} from 'genkitx-groq';
-import { llama3 } from 'genkitx-groq';
+import {groq, llama3x8b} from 'genkitx-groq';
 
 
 export const ai = genkit({
@@ -9,5 +8,5 @@ export const ai = genkit({
       apiKey: process.env.GROQ_API_KEY,
     }),
   ],
-  model: 'llama3-8b-8192',
+  model: llama3x8b,
 });
